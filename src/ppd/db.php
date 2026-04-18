a01fd47da4ad80a7f0c780ccc79abcb5769143dc
Saudi <saudianayatin@gmail.com>
2026-04-18 09:10:59 +0800

    first commit

---
+++ src/ppd/db.php
@@ -0,0 +1,10 @@
+<?php
+	$conn = mysqli_connect("mysql-db", "root", "root", "db_sfms");
+	// $conn = mysqli_connect("localhost", "root", "", "db_sfms");
+	// $conn = mysqli_connect("sql211.epizy.com", "epiz_33822298", "irnqVwStxJOGjSx", "epiz_33822298_mpwfile123");
+	
+	if(!$conn){
+		die("Error: Failed to connect to database!");
+	}
+	
+?>
